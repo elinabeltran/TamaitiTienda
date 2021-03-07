1c962af57c8dc87a1627d553d4cf8e72e25e153b
Elina <elinabeltran@gmail.com>
2021-03-07 19:05:29 -0300

    cambios database.js

--- src/database/config/database.js
+++ src/database/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "db_tamaiti",
-    "host": "127.0.0.1",
+  "production": {
+    "username": "elina",
+    "password": "pacaPACA01!",
+    "database": "elina_tamaiti_ddbb",
+    "host": "mysql-elina.alwaysdata.net",
     "dialect": "mysql"
   },
   "test": {
@@ -13,7 +13,7 @@ module.exports = {
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
-  "production": {
+  "development": {
     "username": "root",
     "password": null,
     "database": "database_production",
